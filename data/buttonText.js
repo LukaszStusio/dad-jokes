0d2a0ae607192cec0ce7b8239c2a994f423e265c
LukaszStusio <lukasz.stusio@gmail.com>
2021-04-27 00:03:54 +0200

    refactored into modules

---
+++ data/buttonText.js
@@ -0,0 +1,13 @@
+const buttonText = [
+    'Ugh...',
+    'Dad, please...',
+    'Oh come on...',
+    'Seriously...',
+    'Stop it!',
+    'Please stop it...',
+    'I hope that was the worst one...',
+    'I\'m dying inside...',
+];
+
+// Default export - only one per file
+export default buttonText;
